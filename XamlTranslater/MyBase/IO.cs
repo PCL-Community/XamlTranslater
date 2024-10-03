7ffdb5487e3d7280a17292867dd872809bdb836b
tangge233 <tangge233@qq.com>
2024-10-03 19:44:03 +0800

    feat: 修改实现1/∞

---
+++ XamlTranslater/MyBase/IO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlTranslater.MyBase
+{
+    internal class IO
+    {
+        public static string GetFileNameFromPath(string path)
+        {
+            string[] paths = path.Split('\\');
+            return paths.Last();
+        }
+    }
+}
---
+++ XamlTranslater/MyXamlManager/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamlTranslater.MyBase;
+
+namespace XamlTranslater.MyXamlManager
+{
+    /// <summary>
+    /// 批量操作
+    /// </summary>
+    internal class Operation
+    {
+        public static void Clear()
+        {
+            FileInfo.Files.Clear();
+        }
+        public static void Add(string DocFile)
+        {
+            FileInfo.Files.Add(new FileInfo(DocFile) );
+        }
+    }
+}
