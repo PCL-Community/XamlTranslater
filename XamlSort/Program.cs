0f23f6276aa2a13f11218d3adca7085a9563a208
tangge233 <tangge233@qq.com>
2024-10-20 09:05:41 +0800

    feat: 可自定义排序的 xaml

--- XamlSort/Program.cs
+++ XamlSort/Program.cs
@@ -12,25 +12,41 @@ namespace XamlSorter
         {
             // XAML文件的路径
             string inputFilePath = "input.xaml";
+            // XAML样板文件路径
+            string exampleFilePath = "example.xaml";
+            List<string> CorrectKeyOrder = new List<string>();
             // 输出文件的路径
             string outputFilePath = "sortedOutput.xaml";
 
-            // 加载XAML文件
-            XDocument xamlDoc = XDocument.Load(inputFilePath);
+            XDocument xamlExampleDoc = XDocument.Load(exampleFilePath);
+            XElement xamlExampleRoot = xamlExampleDoc.Root;
 
-            // 获取XAML的根元素
+            XDocument xamlDoc = XDocument.Load(inputFilePath);
             XElement xamlRoot = xamlDoc.Root;
 
             // 定义XAML的命名空间
             XNamespace xNamespace = "http://schemas.microsoft.com/winfx/2006/xaml";
 
             // 查找所有包含x:Key属性的元素
+            var elementsExampleWithKeys = xamlExampleRoot.Descendants()
+                .Where(e => e.Attributes().Any(a => a.IsNamespaceDeclaration || a.Name.Namespace == xNamespace && a.Name.LocalName == "Key"))
+                .ToList();
+            foreach (var item in elementsExampleWithKeys)
+            {
+                CorrectKeyOrder.Add(item.Attribute(xNamespace+"Key")?.Value);
+            }
+
+            // 查找所有包含x:Key属性的元素
             var elementsWithKeys = xamlRoot.Descendants()
                 .Where(e => e.Attributes().Any(a => a.IsNamespaceDeclaration || a.Name.Namespace == xNamespace && a.Name.LocalName == "Key"))
                 .ToList();
 
+            List<XElement> sortedElements = new List<XElement>();
             // 根据x:Key属性对元素进行排序
-            var sortedElements = elementsWithKeys.OrderBy(e => e.Attribute(xNamespace + "Key")?.Value);
+            foreach (var item in CorrectKeyOrder)
+            {
+                sortedElements.Add(elementsWithKeys.Where(e => e.Attribute(xNamespace + "Key").Value.Equals(item)).First());
+            }
 
             // 创建一个新的XElement，用于构建排序后的XAML
             XElement sortedXamlRoot = new XElement(xamlRoot.Name,
